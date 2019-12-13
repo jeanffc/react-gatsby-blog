41163441ef2f72334f66e9bc8a12d3ecf1dee36f
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-13 19:11:04 -0300

    feat: added Header.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
 
-export default ({ title }) => (
-  <header className="header">
-    <h1 className="header-title">{title}</h1>
-  </header>
+import "./Header.css";
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query HeaderQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <header className="header">
+        <Link to="/">
+          <h1 className="header-title">{data.site.siteMetadata.title}</h1>
+        </Link>
+      </header>
+    )}
+  />
 );
