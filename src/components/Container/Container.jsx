1892f9c93b2204543cb5acf84cf2fd5d6fc4c572
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-14 13:46:45 -0300

    feat: added Container.jsx and Container.css

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "./Container.css";
+
+export default ({ children }) => <div className="container">{children}</div>;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../Header";
+import Container from "../Container";
 
 import "./Layout.css";
 
 export default ({ children, title }) => (
   <div>
     <Header title={title} />
-    <main role="main">{children}</main>
+    <main role="main">
+      <Container>{children}</Container>
+    </main>
   </div>
 );
