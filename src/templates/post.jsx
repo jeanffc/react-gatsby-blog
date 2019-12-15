5ce493b826332f720d8761a113e6f5b8a6966309
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-15 16:30:17 -0300

    feat: added Post Component and Post Template

---
+++ src/templates/post.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default ({ data }) => (
+  <Layout>
+    <p>{/* Aqui vai todo o conteúdo do seu post */}</p>
+  </Layout>
+);
---
+++ src/templates/tag.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default ({ data }) => (
+  <Layout>
+    <p>{/* Aqui vamos listar os posts com determinada tag */}</p>
+  </Layout>
+);
