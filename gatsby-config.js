eab8b6cbe6e64cf7a3ae8e07b2e130eac2535c3c
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-15 16:07:47 -0300

    feat: added plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     title: "React Gatsby Blog",
     description: "O melhor blog de programação do Brasil"
-  }
+  },
+  plugins: [
+    "gatsby-plugin-catch-links",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/content/posts`
+      }
+    }
+  ]
 };
