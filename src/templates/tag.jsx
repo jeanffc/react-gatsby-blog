b1181877548ffcf59ee0dc28f46e7abf7f3a8aab
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-15 16:37:59 -0300

    feat: added Tag Template

--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import Posts from "../components/Posts";
 
-export default ({ data }) => (
-  <Layout>
-    <p>{/* Aqui vamos listar os posts com determinada tag */}</p>
-  </Layout>
-);
+export default class Tag extends Component {
+  render() {
+    const { data, pathContext } = this.props;
+    const { tag } = pathContext;
+    return (
+      <Layout>
+        <div>
+          <h2 style={{ fontSize: "22px", padding: "0px 15px" }}>
+            Todos os posts com a tag: {tag}
+          </h2>
+          <Posts data={data} />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+  query TagPage($tag: String) {
+    allMarkdownRemark(
+      limit: 1000
+      sort: { fields: [fields___prefix], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] }, draft: { ne: true } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "DD/MM/YYYY")
+            description
+          }
+        }
+      }
+    }
+  }
+`;
