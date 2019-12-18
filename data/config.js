a1f106a7855925dee55f8bce9c801b48babbd0e9
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-12-18 10:06:26 -0300

    feat: added data/config.js

---
+++ data/config.js
@@ -0,0 +1,13 @@
+const config = {
+  siteTitle: "React Gatsby Blog",
+  siteDescription: "O melhor blog de programação do Brasil.",
+  siteTitleShort: "React Gatsby Blog",
+  siteRss: "/rss.xml",
+  siteImage: "https://***.com/***.png",
+  googleAnalyticsID: "**-********-*",
+  themeColor: "#000000",
+  backgroundColor: "#F2F3F4",
+  siteUrl: "https://***.com"
+};
+
+module.exports = config;
